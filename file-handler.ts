fcb3ad8c38d11a7d3be46743d9a2af1a5b018e6a
Brian Parrish <brian.parrish3739@gmail.com>
2023-06-15 17:05:47 -0400

    add process args for year and month

--- file-handler.ts
+++ file-handler.ts
@@ -1,7 +1,14 @@
-import { promises as fsPromises } from "fs";
+import { promises as fsPromises, rename } from "fs";
+import * as _ from 'lodash';
+import * as chalk from 'chalk';
+import { Command } from 'commander';
+import { MONTH_LIST } from "./constants";
 
+const DOWNLOADS_DIR = `/Users/brianparrish/Downloads`
+const FINANCE_DIR = `/Users/brianparrish/Documents/Finance`;
+ 
 async function getFileFromDownloads(fileName) {
-  const files = await fsPromises.readdir(`/Users/brianparrish/Downloads`);
+  const files = await fsPromises.readdir(DOWNLOADS_DIR);
   const matches = files.filter((file) => file === fileName);
   if (matches.length > 1) throw new Error(`more than one download matching ${fileName}`)
   else if (matches.length === 0) throw new Error(`could not find any downloads matching ${fileName}`);
@@ -15,15 +22,34 @@ async function checkForFiles() {
   await getFileFromDownloads("activity.csv").catch(handleErr)
   await getFileFromDownloads("Chase9859.csv").catch(handleErr);
   if (errors.length) {
-    throw new Error(errors.join(", "))
+    throw new Error(errors.join("\n"))
   } else {
     return true;
   }
 }
 
+async function makeDirForMonth(year: string, month: string) {
+  const yearDir = `${FINANCE_DIR}/${year}`;
+  const monthNum = MONTH_LIST.indexOf(month.toLowerCase()) + 1;
+  const monthDirname = `${monthNum} - ${_.capitalize(month)}`;
+  const newDir = `${yearDir}/${monthDirname}`
+  return fsPromises.mkdir(newDir).then(() => newDir)
+}
+
 async function main() {
-  const files = await checkForFiles();
-  console.log('files', files);
+  const program = new Command();
+  program
+    .requiredOption('-y, --year <year>', 'year directory')
+    .requiredOption('-m, --month <month>', 'month directory')
+  program.parse(process.argv);
+  const options = program.opts();
+  // const files = await checkForFiles().catch((err) => {
+  //   console.error(chalk.red(`ERRORS:\n`, err.message));
+  //   process.exit(0)
+  // });
+  const newMonthDir = await makeDirForMonth(options.year, options.month)
+  console.log('dir', newMonthDir);
+  // console.log('files', files);
 }
 
 main();
